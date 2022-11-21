792608fadb0c57796d54a0305986ec77ff4f1306
PatrikSzivos0528 <szivospatrikdavid@gmail.com>
2022-11-21 16:57:38 +0100

    main

---
+++ saját/analog óra/analogóra.py
@@ -0,0 +1,18 @@
+from cProfile import label
+from tkinter import *
+from tkinter.ttk import *
+
+from time import time
+import turtle
+
+screen = turtle.Screen()
+t = turtle.Turtle()
+
+screen.title("Clock")
+
+myTurtle = turtle.Turtle()
+myTurtle.circle(140)
+
+
+
+screen.mainloop()
