6d2cec34a6207cb86142aa774a9da08088e5c3c2
PatrikSzivos0528 <szivospatrikdavid@gmail.com>
2022-11-22 14:05:52 +0100

    main

---
+++ 2022.11.08/pyhton feladatok/4.9/10.py
@@ -0,0 +1,27 @@
+import turtle
+
+Screen = turtle.Screen()
+Teknos = turtle.Turtle()
+
+a = turtle.Screen()
+a.bgcolor("lightgreen")
+Teknos.color("black")
+
+Teknos.speed(20)
+for i in range(5):
+    Teknos.pensize(3)
+    Teknos.forward(100)
+    Teknos.right(144)
+    Teknos.forward(100)
+    Teknos.right(144)
+    Teknos.forward(100)
+    Teknos.right(144)
+    Teknos.forward(100)
+    Teknos.right(144)
+    Teknos.forward(100)    
+    Teknos.penup()
+    Teknos.forward(650)
+    Teknos.pendown()
+
+Screen.mainloop()
+
